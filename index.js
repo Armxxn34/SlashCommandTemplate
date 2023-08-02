69476c94a6cd7ea600f49b39829415e87098b4af
Armxxn34 <73848907+Armxxn34@users.noreply.github.com>
2023-08-02 12:01:29 +0100

    Create index.js

---
+++ index.js
@@ -0,0 +1,25 @@
+const Discord = require('discord.js')
+const fs = require('fs');
+const { Client, GatewayIntentBits, Partials, Collection } = require('discord.js');
+const { token, guildId } = require('./config.json');
+
+// Put your own intents and stuff here
+const client = new Client({ intents: [GatewayIntentBits.Guilds], partials: [Partials.Channel] }); 
+
+
+client.commands = new Collection();
+
+const eventFiles = fs.readdirSync('./events').filter(file => file.endsWith('.js'));
+
+for (const file of eventFiles) {
+    const event = require(`./events/${file}`);
+    if (event.once) {
+        client.once(event.name, (...args) => event.execute(client, ...args));
+    } else {
+        client.on(event.name, (...args) => event.execute(client, ...args));
+    }
+}
+
+
+
+client.login(token);
